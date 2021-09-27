5721ae1815a239ec92ace1a92854eb47405c8553
Kihikimaros <82409361+Kihikimaros@users.noreply.github.com>
2021-09-27 20:14:54 +0500

    Initial commit

---
+++ project_collision/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import okhttp3.MediaType.Companion.*;
+import okhttp3.OkHttpClient;
+
+public class Test {
+
+    public static void main(String[] args) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+    }
+}
